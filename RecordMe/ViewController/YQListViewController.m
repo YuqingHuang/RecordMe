e2186712664be483e5ec4a4340d5996d3cae36cc
Yuqing <yqhuang@thoughtworks.com>
2013-12-03 16:38:49 +0800

    hide navigation bar for main VC.

--- RecordMe/ViewController/YQListViewController.m
+++ RecordMe/ViewController/YQListViewController.m
@@ -25,16 +25,9 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
 }
 
 #pragma table view data source methods
